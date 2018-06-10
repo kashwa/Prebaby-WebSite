3e1a13f34c2bb68a1534eaeffed6ac7d988b72d9
kashwa <kashwa80@gmail.com>
2018-06-10 16:44:20 +0200

    Upload baby template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,6 +68,7 @@
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
+                    {{-- Means if user is authenticated. --}}
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else

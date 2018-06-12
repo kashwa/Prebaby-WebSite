cdbd3d225e3376f6faebf4fbf5e8980cf120bc66
kashwa <kashwa80@gmail.com>
2018-06-12 10:02:46 +0200

    set default view for article pages

---
+++ resources/views/firstArticles.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+    <title>First Trimester</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- navbar --}}
+    <div class="w3-top">
+        <div class="w3-bar w3-white w3-card" id="myNavbar">
+            <a href="#home" class="w3-bar-item w3-button w3-wide">Hey Baby</a>
+            <!-- Right-sided navbar links -->
+            <div class="w3-right w3-hide-small">
+            <a href="/" class="w3-bar-item w3-button" style="color:#0f7e9b"><i class="fa fa-home"></i> HOME</a>
+            </div>
+        </div>
+    </div>
+    <style>
+    body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+    body, html {
+        height: 100%;
+        line-height: 1.8;
+    }
+
+    .w3-bar .w3-button {
+        padding: 16px;
+    }
+    </style>
+        
+
+
+    <body>
+            {{-- Insert some content here --}}
+
+            {{-- TAKE BUILDING FROM CORP LARAVEL PROJECT </GITHUB> --}}
+
+        <div class="w3-container" style="padding:128px 16px" id="about">
+        <h3 class="w3-center" style="color: #0f7e9b">FIRST TRIMESTER ARTICLES'</h3>
+        <p class="w3-center w3-large" style="color: #0f7e9b">IN THIS PERIOD OF TIME</p>
+        <div class="w3-row-padding w3-center" style="margin-top:64px">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio soluta quae labore sequi dolore nulla! Cupiditate tempore ipsam nostrum tenetur, explicabo enim molestiae modi, et, porro reiciendis dignissimos facilis.</p>
+        </div>
+        </div>
+            
+
+        
+
+            
+        <!-- Footer -->
+        <footer class="w3-center w3-blue-grey w3-padding-64">
+            <div class="w3-xlarge w3-section">
+                <i class="fa fa-facebook-official w3-hover-opacity"></i>
+                <i class="fa fa-instagram w3-hover-opacity"></i>
+                <i class="fa fa-snapchat w3-hover-opacity"></i>
+                <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+                <i class="fa fa-twitter w3-hover-opacity"></i>
+                <i class="fa fa-linkedin w3-hover-opacity"></i>
+            </div>
+            <p>&copy; 2018 Publisher Team, Faculty of computers and information (Egypt, menoufia), All rights reserved.</p>
+        </footer>
+
+    </body>
+</html>

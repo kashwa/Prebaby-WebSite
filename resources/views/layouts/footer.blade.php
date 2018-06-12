77f63e6af4e06481bcb7d5db5a562619425ca652
kashwa <kashwa80@gmail.com>
2018-06-12 08:05:04 +0200

    add Facebook page link

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,8 @@
 <footer class="w3-center w3-blue-grey w3-padding-64">
   <a href="#home" class="w3-button w3-light-grey"><i class="fa fa-arrow-up w3-margin-right"></i>To the top</a>
   <div class="w3-xlarge w3-section">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <a href="https://www.facebook.com/Hey-Baby-app-1489724987750123/" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+    
     <i class="fa fa-instagram w3-hover-opacity"></i>
     <i class="fa fa-snapchat w3-hover-opacity"></i>
     <i class="fa fa-pinterest-p w3-hover-opacity"></i>

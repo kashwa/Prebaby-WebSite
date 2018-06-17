fee308a315b6bedfdce4a3518a7b15eebff12358
kashwa <kashwa80@gmail.com>
2018-06-17 17:23:42 +0200

    add article creation strategy

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,12 +7,42 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    // public function articleCreateArticle(Request $request){
+    /**
+     * The Logic to create a post.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function articleCreateArticle(Request $request){
+        $request->validate([
+    		'body' 		=>	'required|max:255',
+    		'title'		=>	'required',
+    		'trimester'	=>	'digits_between:0,1|numeric|in:1,2,3'
+        ]);
+        
+        $article = new Article();
 
-    // }
+        $article->body = $request['body']; # Get the body from the form and save it.
+        $article->title = $request['title'];
+        $article->trimester = $request['trimester'];
 
-    public function first(){
+        $article->save();
+         return Redirect()->back()->with('success', 'Article Created');
+    }
+
+    public function index1(){
         $articles = Article::where('trimester',1)->get();
         return view('firstArticles', ['articles' => $articles]);
     }
+
+    public function index2(){
+        $articles = Article::where('trimester',2)->get();
+        return view('firstArticles', ['articles' => $articles]);
+    }
+
+    public function index3(){
+        $articles = Article::where('trimester',3)->get();
+        return view('firstArticles', ['articles' => $articles]);
+    }
+
 }

6f038d808f65696bd344ea0ef79fe701bedb5484
kashwa <kashwa80@gmail.com>
2018-06-17 10:42:19 +0200

    set relationship between article and admins

---
+++ app/Article.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Prebaby;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    // Make relation between Articles and Admins
+    public function admin() {
+        return $this->belongsTo('App\User');
+    }
+}

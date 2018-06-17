f70d65ceeb9b035f1b638a3b45a09b8d3da00e57
kashwa <kashwa80@gmail.com>
2018-06-17 17:23:54 +0200

    set up error validating and success session

--- resources/views/layouts/postArticle.blade.php
+++ resources/views/layouts/postArticle.blade.php
@@ -5,13 +5,12 @@
             <header>
                 <h3>Create an Article</h3><br>
 
-                <form action="">
+                <form action="{{ route('article.create') }}" method="POST">
                     <div class="form-group">
-                        <input type="email" class="form-control" name="new-title" id="new-title" placeholder="Enter a Title" style="margin-bottom: 7px">
-                        <textarea class="form-control" name="new-article" id="new-article" rows="5" placeholder="Write new Article">
-
-                        </textarea>
-                        <input type="text" name="new-trimester" id="new-trimester" placeholder="    Enter Trimester">
+                        <input type="text" class="form-control" name="title" id="new-title" placeholder="Enter a Title" style="margin-bottom: 7px">
+                        <textarea class="form-control" name="body" id="new-article" rows="5" placeholder="Write new Article"></textarea>
+                        <input type="text" name="trimester" id="new-trimester" placeholder="    Enter Trimester">
+                        <input type="hidden" value="{{ Session::token() }}" name="_token">
                         <button type="submit" class="btn btn-primary" style="margin: 7px">Post</button>
                     </div>
                 </form>
@@ -19,3 +18,17 @@
         </div>
     </section>
 </div>
+
+@if(count($errors) > 0)
+	@foreach($errors -> all() as $error)
+		<div class="alert alert-danger">
+			{{$error}}
+		</div>
+	@endforeach
+@endif
+
+@if(session('success'))
+	<div class="alert alert-success">
+		<strong>Successfully!</strong> {{session('success')}}
+	</div>
+@endif
